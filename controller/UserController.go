b8bd9bd71d8d607820c8b2b62a06492cd3018d5f
hayden <mfkhao2009@outlook.com>
2020-03-07 21:13:20 +0800

    add CORSMiddleware

--- controller/UserController.go
+++ controller/UserController.go
@@ -15,10 +15,13 @@ import (
 
 func Register(ctx *gin.Context) {
 	DB := common.GetDB()
+	var requestUser = model.User{}
+	ctx.Bind(&requestUser)
 	// 获取参数
-	name := ctx.PostForm("name")
-	telephone := ctx.PostForm("telephone")
-	password := ctx.PostForm("password")
+	name := requestUser.Name
+	telephone := requestUser.Telephone
+	password := requestUser.Password
+
 	// 数据验证
 	if len(telephone) != 11 {
 		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
@@ -55,8 +58,16 @@ func Register(ctx *gin.Context) {
 	}
 	DB.Create(&newUser)
 
-	//返回结果
-	response.Success(ctx, nil, "注册成功")
+	// 发放token
+	token, err := common.ReleaseToken(newUser)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
+		log.Printf("token generate error : %v", err)
+		return
+	}
+
+	// 返回结果
+	response.Success(ctx,  gin.H{"token": token}, "注册成功")
 }
 
 func Login(ctx *gin.Context)  {
--- routes.go
+++ routes.go
@@ -7,6 +7,7 @@ import (
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
+	r.Use(middleware.CORSMiddleware())
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login", controller.Login)
 	r.GET("api/auth/info", middleware.AuthMiddleware(), controller.Info)
